ceacdbb026c73160896624022190aafcf4f3bc6d
Marsidi <marsidzyberi9@gmail.com>
2024-05-27 19:54:11 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'polis-healthy-hospital',
+  webDir: 'www'
+};
+
+export default config;
