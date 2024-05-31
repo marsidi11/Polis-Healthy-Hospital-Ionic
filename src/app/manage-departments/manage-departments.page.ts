295cda6cdcd995170785cfca48318c2f8d1142b9
Marsidi <marsidzyberi9@gmail.com>
2024-05-31 13:32:38 +0200

    department page

--- src/app/manage-departments/manage-departments.page.ts
+++ src/app/manage-departments/manage-departments.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentService } from '../services/department.service';
+import { AlertController, ModalController } from '@ionic/angular';
+
+export interface Department {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-manage-departments',
@@ -7,15 +13,134 @@ import { DepartmentService } from '../services/department.service';
   styleUrls: ['./manage-departments.page.scss']
 })
 export class ManageDepartmentsPage implements OnInit {
-  departments: any[] = [];
+  departments: Department[] = [];
+  filteredDepartments: Department[] = [];
+  searchTerm: string = '';
 
-  constructor(private departmentService: DepartmentService) {}
+  constructor(
+    private departmentService: DepartmentService,
+    private alertController: AlertController,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
     this.getDepartments();
   }
 
   getDepartments(): void {
-    this.departmentService.getDepartments().subscribe(departments => this.departments = departments);
+    this.departmentService.getDepartments().subscribe(departments => {
+      this.departments = departments;
+      this.filteredDepartments = departments;
+    });
+  }
+
+  filterDepartments() {
+    if (this.searchTerm.trim() === '') {
+      this.filteredDepartments = this.departments;
+    } else {
+      this.filteredDepartments = this.departments.filter(department =>
+        department.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  }
+
+  async openCreateModal() {
+    const alert = await this.alertController.create({
+      header: 'Create Department',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Department Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Create',
+          handler: data => {
+            this.createDepartment(data.name);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async openEditModal(department: Department) {
+    const alert = await this.alertController.create({
+      header: 'Edit Department',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          value: department.name,
+          placeholder: 'Department Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.editDepartment(department.id, data.name);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  createDepartment(name: string) {
+    const newDepartment: Department = { id: this.departments.length + 1, name };
+    this.departmentService.createDepartment(newDepartment).subscribe(department => {
+      this.departments.push(department);
+      this.filterDepartments(); // Refresh the filtered list
+    });
+  }
+
+  editDepartment(id: number, name: string) {
+    const department = this.departments.find(dep => dep.id === id);
+    if (department) {
+      department.name = name;
+      this.departmentService.editDepartment(department).subscribe(() => {
+        this.filterDepartments(); // Refresh the filtered list
+      });
+    }
+  }
+
+  async deleteDepartment(department: Department) {
+    const alert = await this.alertController.create({
+      header: 'Confirm Delete',
+      message: `Are you sure you want to delete the department ${department.name}?`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.departmentService.deleteDepartment(department.id).subscribe(() => {
+              this.departments = this.departments.filter(dep => dep.id !== department.id);
+              this.filterDepartments(); // Refresh the filtered list
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,30 @@ export class DepartmentService {
     return this.http.get<any>(this.dataUrl).pipe(
       map(data => data.departments),
       catchError(this.handleError<any[]>('getDepartments', []))
+    );
+  }
+
+  createDepartment(department: any): Observable<any> {
+    // Here you would normally post to the backend
+    return of(department).pipe(
+      tap(newDepartment => console.log(`created department w/ id=${newDepartment.id}`)),
+      catchError(this.handleError<any>('createDepartment'))
+    );
+  }
+
+  editDepartment(department: any): Observable<any> {
+    // Here you would normally put to the backend
+    return of(department).pipe(
+      tap(_ => console.log(`updated department id=${department.id}`)),
+      catchError(this.handleError<any>('editDepartment'))
+    );
+  }
+
+  deleteDepartment(id: number): Observable<any> {
+    // Here you would normally delete from the backend
+    return of({ id }).pipe(
+      tap(_ => console.log(`deleted department id=${id}`)),
+      catchError(this.handleError<any>('deleteDepartment'))
     );
   }
 
